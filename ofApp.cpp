0ba0d0f3319c9b07fd0e5f247e503e2c94e0ef66
NAKAUCHI Kiyoshi <junkiyoshi@gmail.com>
2024-01-12 21:03:34 +0900

    Add files via upload

---
+++ ofApp.cpp
@@ -0,0 +1,84 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+
+	ofSetFrameRate(25);
+	ofSetWindowTitle("openFrameworks");
+
+	ofBackground(239);
+	ofEnableBlendMode(ofBlendMode::OF_BLENDMODE_SUBTRACT);
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+
+	ofSeedRandom(39);
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+
+	float span = 0.5;
+	vector<ofColor> color_list = { ofColor(0, 255, 255), ofColor(255, 0, 255), ofColor(255, 255, 0)};
+	for (int i = 0; i < color_list.size(); i++) {
+
+		ofMesh mesh;
+		auto noise_value = ofRandom(1000);
+
+		for (float x = 10; x < ofGetWindowWidth() - 10 - span; x += span) {
+
+			for (float y = 10; y < ofGetWindowHeight() - 10 - span; y += span) {
+
+				if (glm::distance(glm::vec2(x, y), ofGetWindowSize() * 0.5) > 300) {
+
+					continue;
+				}
+
+				mesh.addVertex(glm::vec3(x, y, 0));
+				mesh.addVertex(glm::vec3(x + span, y, 0));
+				mesh.addVertex(glm::vec3(x + span, y + span, 0));
+				mesh.addVertex(glm::vec3(x, y + span, 0));
+
+				for (int k = mesh.getNumVertices() - 4; k < mesh.getNumVertices(); k++) {
+
+					auto noise_value = ofNoise(mesh.getVertex(k).x * 0.008, mesh.getVertex(k).y * 0.008, ofGetFrameNum() * 0.015 + i * 0.05) ;
+					int int_noise_value = noise_value;
+					float param = noise_value - int_noise_value;
+					param = abs(param - 0.5);
+
+					auto color = ofColor(color_list[i], param < 0.125 ? ofMap(param, 0, 0.125, 255, 0) : 0);
+					mesh.addColor(color);
+				}
+
+				mesh.addIndex(mesh.getNumVertices() - 1); mesh.addIndex(mesh.getNumVertices() - 2); mesh.addIndex(mesh.getNumVertices() - 3);
+				mesh.addIndex(mesh.getNumVertices() - 1); mesh.addIndex(mesh.getNumVertices() - 3); mesh.addIndex(mesh.getNumVertices() - 4);
+			}
+		}
+
+		mesh.draw();
+	}
+
+	/*
+	int start = 1;
+	if (ofGetFrameNum() > start) {
+
+		ostringstream os;
+		os << setw(4) << setfill('0') << ofGetFrameNum() - start;
+		ofImage image;
+		image.grabScreen(0, 0, ofGetWidth(), ofGetHeight());
+		image.saveImage("image/cap/img_" + os.str() + ".jpg");
+		if (ofGetFrameNum() - start >= 25 * 20) {
+
+			std::exit(1);
+		}
+	}
+	*/
+}
+
+//--------------------------------------------------------------
+int main() {
+
+	ofSetupOpenGL(720, 720, OF_WINDOW);
+	ofRunApp(new ofApp());
+}
